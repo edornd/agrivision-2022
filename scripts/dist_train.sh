4239bf00c54bda6595b470fe4ea751d56d1652a1
Edoardo Arnaudo <edoardo.arn@gmail.com>
2022-02-27 14:57:29 +0000

    Customize DDP script

--- scripts/dist_train.sh
+++ scripts/dist_train.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-CONFIG=$1
-GPUS=$2
+CUDA=$1
+CONFIG=$2
+PROC=$(awk -F',' '{print NF}' <<< "$CUDA")
 PORT=${PORT:-29501}
 
 DATE=$(date +%Y-%m-%d)
 SALT=$(openssl rand -base64 6)
-echo "${DATE}_${SALT}"
+echo "Experiment: ${DATE}_${SALT}"
+echo "Devices:    $CUDA"
+echo "Num.Proc:   $PROC"
+echo "Port:       $PORT"
 
 # PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
-    tools/train.py $CONFIG --launcher pytorch --suffix "${DATE}_${SALT}" ${@:3}
+CUDA_VISIBLE_DEVICES=$CUDA python -m torch.distributed.run --nproc_per_node=$PROC --master_port=$PORT \
+    tools/train.py $CONFIG --launcher pytorch --suffix "${DATE}_${SALT}" ${@:4}
